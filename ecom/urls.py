9d831431529995a175cf0c5a2611f5c6211f0d18
Mambodiev <mambodieu@gmail.com>
2021-05-30 15:50:54 +0300

    language_static

--- ecom/urls.py
+++ ecom/urls.py
@@ -16,18 +16,18 @@ urlpatterns = [
 
 urlpatterns += i18n_patterns(
     path(_('admin/'), admin.site.urls),
-    path('accounts/', include('allauth.urls')),
+    path(_('accounts/'), include('allauth.urls')),
     path('', views.home, name='home'),
     path(_('about/'), views.about, name='about'),
-    path('privacy_policy/', views.privacy_policy, name='privacy_policy'),
-    path('shipping_returns/', views.shipping_returns, name='shipping_returns'),
+    path(_('privacy_policy/'), views.privacy_policy, name='privacy_policy'),
+    path(_('shipping_returns/'), views.shipping_returns, name='shipping_returns'),
     path(_('contact/'), views.ContactView.as_view(), name='contact'),
-    path('faq/', views.faq, name='faq'),
-    path('terms_of_use/', views.terms_of_use, name='terms_of_use'),
-    path('cart/', include('cart.urls', namespace='cart')),
-    path('staff/', include('staff.urls', namespace='staff')),
-    path('profile/', views.ProfileView.as_view(), name='profile'),
-    path('ckeditor/', include('ckeditor_uploader.urls')),
+    path(_('faq/'), views.faq, name='faq'),
+    path(_('terms_of_use/'), views.terms_of_use, name='terms_of_use'),
+    path(_('cart/'), include('cart.urls', namespace='cart')),
+    path(_('staff/'), include('staff.urls', namespace='staff')),
+    path(_('profile/'), views.ProfileView.as_view(), name='profile'),
+    path('_ckeditor/', include('ckeditor_uploader.urls')),
     prefix_default_language=False,
 )
 

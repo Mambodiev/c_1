7db1a352cd6740839ccf549d533ae870ea484c9a
Diev <Mambodiev>
2021-06-13 09:54:41 -0700

    rosetta

--- cart/views.py
+++ cart/views.py
@@ -53,7 +53,7 @@ class ProductListView(generic.ListView):
     def get_context_data(self, **kwargs):
         context = super(ProductListView, self).get_context_data(**kwargs)
         context.update({
-            "categories": Category.objects.values("name")
+            "categories": Category.objects.values("translations__name")
         })
         return context
 

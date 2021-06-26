e8ad5d75331d18fff7776eb9219fa1be3ccb42c2
Mambodiev <mambodieu@gmail.com>
2021-06-26 16:22:06 -0700

    productslide1

--- static/js/main.js
+++ static/js/main.js
@@ -265,30 +265,30 @@ $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
 // 	} ).mount();
 // } );
 
-document.addEventListener( 'DOMContentLoaded', function () {
-	var secondarySlider = new Splide( '#secondary-slider', {
-		fixedWidth  : 100,
-		height      : 60,
-		gap         : 10,
-		cover       : true,
-		isNavigation: true,
-		focus       : 'center',
-		breakpoints : {
-			'600': {
-				fixedWidth: 66,
-				height    : 40,
-			}
-		},
-	} ).mount();
+// document.addEventListener( 'DOMContentLoaded', function () {
+// 	var secondarySlider = new Splide( '#secondary-slider', {
+// 		fixedWidth  : 100,
+// 		height      : 60,
+// 		gap         : 10,
+// 		cover       : true,
+// 		isNavigation: true,
+// 		focus       : 'center',
+// 		breakpoints : {
+// 			'600': {
+// 				fixedWidth: 66,
+// 				height    : 40,
+// 			}
+// 		},
+// 	} ).mount();
 	
-	var primarySlider = new Splide( '#primary-slider', {
-		type       : 'fade',
-		heightRatio: 0.5,
-		pagination : false,
-		arrows     : false,
-		cover      : true,
-	} ); // do not call mount() here.
+// 	var primarySlider = new Splide( '#primary-slider', {
+// 		type       : 'fade',
+// 		heightRatio: 0.5,
+// 		pagination : false,
+// 		arrows     : false,
+// 		cover      : true,
+// 	} ); // do not call mount() here.
 	
-	primarySlider.sync( secondarySlider ).mount();
-} );
+// 	primarySlider.sync( secondarySlider ).mount();
+// } );
 // end product img
